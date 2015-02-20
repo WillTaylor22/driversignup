89187486668d7be786dcc8f10cd8d30cb1c4f057
Will <wrftaylor@gmail.com>
2015-02-20 03:26:52 +0000

    initial commit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Driversignup::Application.config.secret_key_base = '2febba9d0a13f59fc663a18d1d948aaec22ad7feb2588f2da32770576b9bdbd528cd5df3f6c0548c1d3a26a5649781e2e2019089da31a0cff9a9ed6bfc360e90'
